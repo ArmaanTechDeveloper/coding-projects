a15863202b1fda2325098c95da46e948c5951b9b
ArmaanTechDeveloper <kournarinder948@gmail.com>
2024-12-17 20:58:43 +0530

    second commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center text-3xl">hi there 🚀!</div>
+  );
+}
