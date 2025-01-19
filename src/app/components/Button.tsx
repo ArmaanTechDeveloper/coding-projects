5c32ad6525143b87b986abea5857c30ddcf7d043
ArmaanTechDeveloper <kournarinder948@gmail.com>
2025-01-19 21:48:45 +0530

    v2 added onclick button

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+export function Button() {
+    const [visible , setVisible] = useState(true);
+    function greet(event: React.MouseEvent){
+        setVisible(false);
+    }
+    return(
+        <div>
+            {visible && <button onClick={(e) => greet(e)} className="bg-blue-400 p-2 rounded hover:bg-blue-500 transition">Click me</button>}
+            {!visible && <p className="text-2xl my-2">You are someone special to me 🫣🤗❤️</p>}
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { Button } from "./components/Button";
 
 export default function Home() {
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center text-3xl">hi there 🚀!</div>
+    <div className="w-full h-[100vh] flex justify-center flex-col items-center">
+      <p className="text-5xl my-2">hi there 🚀!</p>
+      <Button />
+    </div>
   );
 }
